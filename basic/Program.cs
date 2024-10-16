a7a73c2003a39fba9bd563aa0c117ceca3ab35c6
Navas <gabrielnavas@protonmail.com>
2024-10-16 10:09:13 -0300

    variables

---
+++ basic/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool isOpen = true;
+char letter = 'a';
+byte number = 255;
+int age = 22;
+long foo = 8999999999999999999;
+float pi = 3.14f;
+double pii = 3.14159265359;
+decimal price = 2.99m;
+string name = "John";
