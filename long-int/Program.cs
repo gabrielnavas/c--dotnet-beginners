e12f8841a9fa88bc5e2713c9f0dbb14c90904998
Navas <gabrielnavas@protonmail.com>
2024-10-16 12:51:36 -0300

    long to int example

---
+++ long-int/Program.cs
@@ -0,0 +1,8 @@
+﻿// não vai converter, pois long não cabe no int
+
+long longNumber = 8_999_999_999_999_998_000;
+int intNumber = (int) longNumber;
+System.Console.WriteLine(intNumber); // -494667728
+
+
+
