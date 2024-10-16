82b2f61fb27d2a31ba0fee0d184bedd1d000fd40
Navas <gabrielnavas@protonmail.com>
2024-10-16 10:52:05 -0300

    basic arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] names = {
+  "John", "Mary"
+};
+Console.WriteLine(names[0]);
+Console.WriteLine(names[1]);
+
+
+string[] fruits = new string[2];
+fruits[0] = "Apple";
+fruits[1] = "Banana";
+
+Console.WriteLine(fruits[0]);
+Console.WriteLine(fruits[1]);
