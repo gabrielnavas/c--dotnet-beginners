accb13f838b67831f18b01549ce11117006e207d
Navas <gabrielnavas@protonmail.com>
2024-10-16 12:56:36 -0300

    references

---
+++ references-types/Program.cs
@@ -0,0 +1,20 @@
+﻿string s = "Mary";
+string s2 = s;
+Console.WriteLine(s2); // Mary
+
+
+int i = 5;
+int j = i;
+j = 50;
+Console.WriteLine(j); // 50
+Console.WriteLine(i); // 5
+
+
+Car bmw = new Car();
+bmw.Name = "bmw";
+
+Console.WriteLine(bmw); // reference to bmw type of Car
+
+class Car {
+ public string Name;
+}
