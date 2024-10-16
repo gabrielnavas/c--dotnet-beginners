cd2bd9b4a88101b9d0789241b444d0694f27f2c5
Navas <gabrielnavas@protonmail.com>
2024-10-16 17:52:14 -0300

    interfaces

---
+++ interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+ILogger log = new Logger("Welcome");
+log.Log();
+
+
+interface ILogger
+{
+  void Log();
+}
+
+class Logger : ILogger
+{
+
+  private string message;
+
+  public Logger(string message)
+  {
+    this.message = message;
+  }
+
+  public void Log()
+  {
+    Console.WriteLine(message);
+  }
+}
