9ad5d9076480adf4f4281f55d1c64401768bd667
Navas <gabrielnavas@protonmail.com>
2024-10-16 10:12:21 -0300

    operators

--- basic/Program.cs
+++ basic/Program.cs
@@ -10,3 +10,16 @@ float pi = 3.14f;
 double pii = 3.14159265359;
 decimal price = 2.99m;
 string name = "John";
+
+
+// operations
+
+if (isOpen)
+{
+  Console.WriteLine("Is Open!");
+}
+
+string ofLegalAge = age >= 18
+  ? "I am of legal age"
+  : "I'm not of legal age";
+Console.WriteLine(ofLegalAge);
