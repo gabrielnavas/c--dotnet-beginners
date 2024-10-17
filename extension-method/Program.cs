4b91a207aa38396e55fd1e5b3173aed818ac391e
Navas <gabrielnavas@protonmail.com>
2024-10-16 21:59:07 -0300

    extension method

---
+++ extension-method/Program.cs
@@ -0,0 +1,10 @@
+﻿"Hello World".Write(ConsoleColor.DarkBlue);
+
+static class ConsoleLine {
+  public static void Write(this string text, ConsoleColor color)
+  {
+    Console.ForegroundColor = color;
+    Console.WriteLine(text);
+    Console.ResetColor();
+  }
+}
