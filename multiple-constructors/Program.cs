fbe1a5689d4cc945e867d2fbe4fc4639383efd89
Navas <gabrielnavas@protonmail.com>
2024-10-17 08:55:38 -0300

    multiple constructos

---
+++ multiple-constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿BankAccount bankAccount = new BankAccount();
+System.Console.WriteLine(bankAccount.Name); // anonymous
+System.Console.WriteLine(bankAccount.Balance); // 0
+
+class BankAccount
+{
+  private string name;
+  private decimal balance;
+
+  public string Name
+  {
+    get
+    {
+      return name;
+    }
+  }
+  public decimal Balance
+  {
+    get
+    {
+      return balance;
+    }
+  }
+
+  public BankAccount(string name, decimal balance)
+  {
+    this.name = name;
+    this.balance = balance;
+  }
+
+  public BankAccount(decimal balance) : this("anonymous", balance)
+  {
+  }
+
+  public BankAccount() : this("anonymous", 0)
+  {
+  }
+}
