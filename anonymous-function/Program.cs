e2156957a9d13d92fec2f3479580d70ade6cd06a
Navas <gabrielnavas@protonmail.com>
2024-10-16 21:38:56 -0300

    lambda

--- anonymous-function/Program.cs
+++ anonymous-function/Program.cs
@@ -1,18 +1,13 @@
 ﻿// Action
-Action<string> log = delegate (string name) {
-  Console.WriteLine(name);
-};
+Action<string> log = (string name) => Console.WriteLine(name);
 log("Hello world"); // "Hello world"
 
 
 // Func
+Func<List<int>, int> calc = (List<int> values) => values.Sum();
 int result = CalculateTax(calc, new List<int> { 2, 4, 6, 8 });
 Console.WriteLine(result); // 20
 
-int calc(List<int> values)
-{
-  return values.Sum();
-}
 
 int CalculateTax(Func<List<int>, int> callback, List<int> values)
 {
--- anonymous-function/obj/Debug/net8.0/anonymous-function.AssemblyInfo.cs
+++ anonymous-function/obj/Debug/net8.0/anonymous-function.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anonymous-function")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9396fa29026c6a69d52643c96492be1abe2fffd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8470ed7c06622ad50eeb014a4d65658b03b0ac76")]
 [assembly: System.Reflection.AssemblyProductAttribute("anonymous-function")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anonymous-function")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
